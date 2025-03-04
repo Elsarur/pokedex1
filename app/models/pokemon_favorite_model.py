7662aaf8c38c9f5a01d0788220a1da8b8f426b8c
Sarur Santoyo Gonzalez <ant.45tr@gmail.com>
2025-03-04 12:30:49 -0600

    Commit 04 Marzo con Error

--- app/models/pokemon_favorite_model.py
+++ app/models/pokemon_favorite_model.py
@@ -1,24 +1,32 @@
-from app import mongo 
+from app import mongo
 from app.models.super_clase import SuperClass
 from bson import ObjectId
 
+#creamos la clase pokemon
 class PokemonFavorites(SuperClass):
     def __init__(self):
-        super().__init__("pokemons_favorites")
+        super().__init__("pokemons")
 
+    def create(self, data):
+        raise NotImplementedError("Los pokemones no se pueden crear")
+    
+    def delete(self, object_id):
+        raise NotImplementedError("Los pokemones no se pueden eliminar")
+    
     def update(self, object_id, data):
-        raise NotImplementedError("Los pokemones favoritos no se pueden actualizar")
+        raise NotImplementedError("Los pokemones no se pueden actualizar")
     
     def find_all(self, user_id):
-        data = self.collection.find({"user_id": ObjectId(user_id)})
-        for datum in data:
-            datum["user_id"]=str(datum["user_id"])
-            datum["pokemon_id"]=str(datum["pokemon_id"])
-            datum["_id"]=str(datum["_id"])
-        return data
+         data = self.collection.find({"user_id": ObjectId(user_id)})
+         for datum in data:
+          datum["user_id"] = str(datum["user_id"])
+          datum["pokemon_id"] = str(datum["pokemon_id"])
+          datum["_id"] = str(datum["_id"])
+
+         raise data
     
     def create(self, data):
-        data["user_id"] = ObjectId(data["user_id"])
-        data["pokemon_id"] = ObjectId(data["pokemon_id"])
-        datum = self.collection.insert_ones(data)
-        return str(datum.inserted_id)
+     data["user_id"] = ObjectId(data["user_id"])
+     data["pokemon_id"] = ObjectId(data["pokemon_id"])
+     datum = self.collection.insert_ones(data)
+     return str(datum.inserted_id)
